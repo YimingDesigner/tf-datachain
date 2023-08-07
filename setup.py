9df123d48015731f0ad3b42cb0b6bb26956eeb9e
Yiming Liu <YimingDesigner@gmail.com>
2023-08-07 20:58:31 +0800

    fix: add long description type

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     description="A local dataset loader based on tf.data input pipeline",
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author="Yiming Liu",
     author_email="YimingDesigner@gmail.com",
     package_dir={'':"src"},
