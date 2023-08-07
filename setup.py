2ca79795034aaef78fc5156d488d09bb776f6a21
Yiming Liu <YimingDesigner@gmail.com>
2023-08-07 20:40:47 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tf_datachain",
+    version="0.1.0",
+    description="A local dataset loader based on tf.data input pipeline",
+    author="Yiming Liu",
+    author_email="YimingDesigner@gmail.com",
+    package_dir={'':"src"},
+    packages=find_packages("src"),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pandas",
+        "tensorflow",
+        "keras-cv",
+        "keras-core",
+        "opencv-python",
+    ],
+)
